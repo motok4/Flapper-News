a3f670bd3eb78476cec9ee702c43768c9140430e
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-09-11 08:53:49 -0400

    Allow user to upload links and to upvote.

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ function($scope){
         if ($scope.title === '') {
             return;
         }
-        $scope.posts.push({title: $scope.title, upvotes: 0});
+        $scope.posts.push({
+            title: $scope.title, 
+            link: $scope.link, 
+            upvotes: 0
+        });
         $scope.title = ''
-    } 
+        $scope.link = ''
+    };
+    $scope.incrementUpvotes = function(post) {
+        post.upvotes += 1
+    }
+
 }])
