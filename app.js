6ee1873da1d92f668585521d4afc32899a357f02
Pei Xiong Liu <peixiong.liu@gmail.com>
2014-09-26 13:23:10 -0400

    Include post controller. App is throwing errors

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ function($stateProvider, $urlRouterProvider) {
             url: '/home',
             templateUrl: '/home.html',
             controller: 'MainCtrl'
-        });
+        })
+
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl'
+        })
 
     $urlRouterProvider.otherwise('home');
 }])
@@ -21,27 +27,72 @@ function($stateProvider, $urlRouterProvider) {
 function($scope, posts){
     $scope.test = 'Hello world!';
     $scope.posts = posts.posts;
+
     $scope.addPost = function() {
         if ($scope.title === '') {
             return;
         }
         $scope.posts.push({
-            title: $scope.title, 
-            link: $scope.link, 
+            title: $scope.title,
+            link: $scope.link,
             upvotes: 0
         });
-        $scope.title = ''
-        $scope.link = ''
+        $scope.title = '';
+        $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
-        post.upvotes += 1
+        post.upvotes += 1;
     }
 
+}])
+
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+    // $scope.posts.push({
+    //     title: $scope.title,
+    //     link: $scope.link,
+    //     upvotes: 0,
+    //     comments: [
+    //         {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    //         {author: 'Bob', body: 'Great idea, but everything is wrong!', upvotes: 1}
+    //     ]
+    // });
+    $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+            body: $scope.body,
+            author: 'user',
+            upvotes: 0
+        });
+        $scope.body = '';
+    };
 }])
 
 .factory('posts', [function(){
     var o = {
         posts: []
     };
-    return o
+    return o;
 }])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
